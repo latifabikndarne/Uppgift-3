016bb4924a25c104132e053ec80207bfe3e4bdc7
latifabikndarne <latifa.bikndarne@gmail.com>
2025-09-19 16:21:45 +0200

    files uppdated

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const myH1 = document.querySelector("#myH1");
 const myButton = document.querySelector("#myButton");
 
 
-//myH1tag.textContent = "Godbye World!";
-
 myButton.addEventListener(
     "click",
     newContent
 );
 
 function newContent() {
-   myH1.textContent = "Godbye World!";
+    myH1.textContent = "Godbye World!";
 }
