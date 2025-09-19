6224524d4600f89ee5f9ffb06191081f39794176
latifabikndarne <latifa.bikndarne@gmail.com>
2025-09-19 13:34:28 +0200

    js file is uppdated

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
 
-const h1 = document.querySelector("Hellow World!");
-const button = document.querySelector("myButton");
-
-button.addEventListener(
-    "click",
-
-    function () {
-
-        h1.changetextContent = "Goodbye World!";
-    }
-
-)
+const myH1tag = document.querySelector("H1");
