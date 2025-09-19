0eccbb4fcc3d3b6683a08c3ccdea8470a5e96f92
latifabikndarne <latifa.bikndarne@gmail.com>
2025-09-19 10:17:52 +0200

    Create js and css files

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const h1 = document.querySelector("Hellow World!");
+const button = document.querySelector("button");
+
+button.addEventListener(
+    "click", 
+
+function () {
+       
+    h1.changetextContent = "Goodbye World!";
+}
+
+)
